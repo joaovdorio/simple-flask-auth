ff470a9bbbe21ad03f1baadbfe0f1574097b8b90
joaovdorio <joaovitor.dorio@gmail.com>
2025-06-24 20:58:29 -0300

    added login validation using flask_login

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "yoursecretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+#view login
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message":"Login realizado com sucesso"}), 200
+            
+    return jsonify({"message":"As credenciais estão inválidas"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
